c3082bb11d7b3a72888bd2c353efdb227c834f2a
Zreal <zenglz_pro@163.com>
2020-09-13 23:49:52 +0800

    proj0;

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.sqrt;
+
 /**
  * @program: skeleton-sp18
  * @description:
@@ -6,6 +8,8 @@
  **/
 
 public class Planet {
+    static final double G=6.67e-11;
+
     public double xxPos;
     public double yyPos;
     public double xxVel;
@@ -26,4 +30,76 @@ public class Planet {
     public Planet(Planet p){
         this(p.xxPos,p.yyPos,p.xxVel,p.yyVel,p.mass,p.imgFileName);
     }
+
+    public double calcDistance(Planet roinate){
+        double delta_x=this.xxPos-roinate.xxPos;
+        double delta_y=this.yyPos-roinate.yyPos;
+        return sqrt(delta_x*delta_x+delta_y*delta_y);
+    }
+
+    public double calcForceExertedBy(Planet roinate){
+        double r=this.calcDistance(roinate);
+        return G*this.mass*roinate.mass/r/r;
+    }
+
+    public double calcForceExertedByX(Planet roinate){
+        double F=this.calcForceExertedBy(roinate);
+        double delta_x=roinate.xxPos-this.xxPos;
+        double r=this.calcDistance(roinate);
+        return F*delta_x/r;
+    }
+
+    public double calcForceExertedByY(Planet roinate){
+        double F=this.calcForceExertedBy(roinate);
+        double delta_y=roinate.yyPos-this.yyPos;
+        double r=this.calcDistance(roinate);
+        return F*delta_y/r;
+    }
+
+    public Boolean equal(Planet roinate){
+        if(this.xxPos==roinate.xxPos && this.yyPos==roinate.yyPos
+                && this.mass==roinate.mass){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public double calcNetForceExertedByX(Planet [] allPlanet){
+        double ForcesByX=0.0;
+        for(Planet roinate:allPlanet){
+            if(this.equal((roinate))) continue;
+            else{
+                double x=this.calcForceExertedByX(roinate);
+                ForcesByX+=x;
+            }
+        }
+        return ForcesByX;
+    }
+
+    public double calcNetForceExertedByY(Planet [] allPlanet){
+        double ForcesByY =0.0;
+
+        for(Planet roinate:allPlanet){
+            if(this.equal((roinate))) continue;
+            else{
+                double y=this.calcForceExertedByY(roinate);
+                ForcesByY+=y;
+            }
+        }
+        return ForcesByY;
+    }
+
+    public void update(double dt,double fx, double fy){
+        double ax=fx/this.mass;
+        double ay=fy/this.mass;
+
+        this.xxVel+=dt*ax;
+        this.yyVel+=dt*ay;
+
+        this.xxPos+=dt*this.xxVel;
+        this.yyPos+=dt*this.yyVel;
+    }
+
 }
