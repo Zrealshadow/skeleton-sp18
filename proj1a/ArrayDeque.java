b57433607c7b26d41d3d6dbdd5548e963c5204c0
Zreal <zenglz_pro@163.com>
2020-09-20 15:30:35 +0800

    ,,.,

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -4,17 +4,17 @@
  * @author: Lingze_Waldinsamkeit
  * @create: 2020-09-17 16:40
  **/
-public class ArrayDeque<T>{
+public class ArrayDeque< T > {
 
     private T[] items;
     private int nextFirst;
     private int nextLast;
-    private int START_SIZE=8;
+    private int START_SIZE = 8;
 //    private int REMOVAL_SIZE=
 
-    public ArrayDeque(){
+    public ArrayDeque() {
         items=(T []) new Object[START_SIZE];
-        nextFirst = items.length-1;
+        nextFirst = items.length - 1;
         nextLast = 0;
     }
 
@@ -25,83 +25,83 @@ public class ArrayDeque<T>{
 //        nextFirst=capacity-items.length+nextLast;
 //        System.arraycopy(items,last_nextFirst,new_items,nextFirst,items.length-nextLast);
 //        items=new_items;
-        int size=this.size();
+        int size = this.size();
         T[] new_items=(T []) new Object[capacity];
-        if(nextFirst<nextLast){
-            System.arraycopy(items,nextFirst+1,new_items,0,nextLast-nextFirst-1);
+        if (nextFirst<nextLast){
+            System.arraycopy(items,nextFirst + 1,new_items,0,nextLast - nextFirst - 1);
         }
-        else{
-            System.arraycopy(items,nextFirst+1,new_items,0,items.length-nextFirst-1);
-            System.arraycopy(items,0,new_items,items.length-nextFirst-2,nextLast);
+        else {
+            System.arraycopy(items,nextFirst + 1,new_items,0,items.length - nextFirst - 1);
+            System.arraycopy(items,0,new_items,items.length - nextFirst - 2,nextLast);
         }
-        items=new_items;
-        nextFirst=items.length-1;
-        nextLast=size;
+        items = new_items;
+        nextFirst = items.length - 1;
+        nextLast = size;
     }
 
-    public void addFirst(T item){
-        if(nextLast==nextFirst){
-            int c=items.length*2;
+    public void addFirst(T item) {
+        if(nextLast == nextFirst){
+            int c = items.length*2;
             resize(c);
         }
-        items[nextFirst]=item;
-        nextFirst=(nextFirst-1+items.length)%items.length;
+        items[nextFirst] = item;
+        nextFirst = (nextFirst-1+items.length) % items.length;
     }
 
-    public void addLast(T item){
-        if(nextLast==nextFirst){
-            int c=items.length*2;
+    public void addLast(T item) {
+        if (nextLast == nextFirst) {
+            int c = items.length*2;
             resize(c);
         }
-        items[nextLast]=item;
-        nextLast=(nextLast+1)%items.length;
+        items[nextLast] = item;
+        nextLast = (nextLast+1)%items.length;
     }
 
-    public boolean isEmpty(){
-        if((nextLast-nextFirst+items.length)%items.length==1)
+    public boolean isEmpty() {
+        if((nextLast-nextFirst+items.length)%items.length == 1)
             return true;
         else
             return false;
     }
 
-    public int size(){
-        if(nextFirst>=nextLast) return nextLast+ items.length-nextFirst-1;
+    public int size() {
+        if(nextFirst >= nextLast) return nextLast+ items.length-nextFirst-1;
         else return nextLast-nextFirst-1;
     }
 
-    public void printDeque(){
-        int i=nextFirst+1;
+    public void printDeque() {
+        int i = nextFirst + 1;
         for(;i!=nextLast;i++){
-            if(i>=items.length) i=i%items.length;
+            if (i >= items.length) i = i % items.length;
             System.out.print(items[i]);
             System.out.print(" ");
         }
     }
 
-    public T removeFirst(){
-        if(this.isEmpty()){
+    public T removeFirst() {
+        if (this.isEmpty()) {
             return null;
         }
-        nextFirst=(nextFirst+1)%items.length;
+        nextFirst = (nextFirst+1) % items.length;
         if(this.size()<items.length/2-2 && items.length>START_SIZE){
             this.resize(items.length/2);
         }
         return items[nextFirst];
     }
 
-    public T removeLast(){
-        if(this.isEmpty()){
+    public T removeLast() {
+        if (this.isEmpty()) {
             return null;
         }
         nextLast=(nextLast-1+items.length)%items.length;
-        if(this.size()<items.length/2-2 && items.length>START_SIZE){
+        if (this.size()<items.length/2-2 && items.length>START_SIZE) {
             this.resize(items.length/2);
         }
         return items[nextLast];
     }
 
-    public T get(int index){
-        int i=(nextFirst+index)%items.length;
+    public T get(int index) {
+        int i = (nextFirst+index)%items.length;
         return items[i];
     }
 
